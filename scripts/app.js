8c3f5505066e0f305f643c0fc2e5670a23f61d90
max-b10 <74245108+max-b10@users.noreply.github.com>
2021-03-18 21:59:58 +0000

    Add files via upload

---
+++ scripts/app.js
@@ -0,0 +1,24 @@
+console.log("app.js running");
+// Operator buttons
+const sumButton = document.querySelector("#sum");
+const subtractButton = document.querySelector("#subtract");
+const multiplyButton = document.querySelector("#multiply");
+const divideButton = document.querySelector("#divide");
+const equalsButton = document.querySelector("#equals");
+const decimalPointButton = document.querySelector("#decimal");
+const clearButton = document.querySelector("#clear");
+
+// Number buttons
+const oneButton = document.querySelector("#one");
+const twoButton = document.querySelector("#two");
+const threeButton = document.querySelector("#three");
+const fourButton = document.querySelector("#four");
+const fiveButton = document.querySelector("#five");
+const sixButton = document.querySelector("#six");
+const sevenButton = document.querySelector("#seven");
+const eightButton = document.querySelector("#eight");
+const nineButton = document.querySelector("#nine");
+
+// Calculator display divs
+const previousOperandDisplay = document.querySelector("#previous-operand");
+const currentOperandDisplay = document.querySelector("#current-operand");
