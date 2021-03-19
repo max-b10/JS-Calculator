ae1f84ad31328b9e834b959aba39907fe3885958
max-b10 <bungaymax@gmail.com>
2021-03-19 10:34:22 +0000

    Update app.js

--- scripts/app.js
+++ scripts/app.js
@@ -1,4 +1,10 @@
-console.log("app.js running");
+// Calculator
+const calculator = document.querySelector(".calculator");
+
+// Display divs
+let secondaryDisplay = document.querySelector("#secondary-display");
+let primaryDisplay = document.querySelector("#primary-display");
+
 // Operator buttons
 const sumButton = document.querySelector("#sum");
 const subtractButton = document.querySelector("#subtract");
@@ -19,6 +25,23 @@ const sevenButton = document.querySelector("#seven");
 const eightButton = document.querySelector("#eight");
 const nineButton = document.querySelector("#nine");
 
-// Calculator display divs
-const previousOperandDisplay = document.querySelector("#previous-operand");
-const currentOperandDisplay = document.querySelector("#current-operand");
+// Looping through and adding event listeners to all number buttons on the calc.
+const calcNumberButtons = document.querySelectorAll(".number");
+for (let i = 0; i < calcNumberButtons.length; i++) {
+  calcNumberButtons[i].addEventListener("click", (e) => {
+    // Cause the primary display to reflect which number button is clicked.
+    let calcNumberButton = e.target;
+    let calcNumberButtonValue = calcNumberButton.textContent;
+    let primaryDisplayValue = primaryDisplay.textContent;
+    // Prevent '0s' being concatenated if it's the first button clicked.
+    if (primaryDisplayValue === "0") {
+      primaryDisplay.textContent = calcNumberButtonValue;
+    } else {
+      primaryDisplay.textContent += calcNumberButtonValue;
+    }
+  });
+}
+// Clear Button
+clearButton.addEventListener("click", (e) => {
+  primaryDisplay.textContent = "";
+});
